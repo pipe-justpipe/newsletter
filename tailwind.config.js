cfc8bde282c50a91c6d19fb8bb7057fa30daf6fa
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-08-29 21:38:53 +0100

    style commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+
     extend: {
       colors:{
         darkgrey:"hsl(234, 29%, 20%)",
         whitee:"hsl(0, 0%, 100%)",
         myGrey: "hsl(231, 7%, 60%)"
-      }
+      },
+      height: {
+        '80':'45%',
+        '100':'50%' 
+      },
     },
   },
   plugins: [],
