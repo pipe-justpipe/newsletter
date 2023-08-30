e545cecf7dac7e59a078a758a98da5794dbe03d7
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-08-30 18:12:58 +0100

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// const btn = document.querySelector('#btn');
+// const messsage  = document.querySelector('.message')
+
+// btn.addEventListener('click', function(){
+//     messsage.classList.remove('hide')
+// })
